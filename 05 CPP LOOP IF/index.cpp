20e5491eacfd2d1795107b3c86acee57a0d67b79
Dharmik Ahir <sangadharmik78@gmail.com>
2025-06-06 11:27:47 +0530

    loop C++

---
+++ 05 CPP LOOP IF/index.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int i=1;
+ 
+
+    int sum = 0;
+
+    while(i<=10){
+        sum=sum+i+;
+        i++;
+    }
+
+    cout<<"Sum: "<<sum;
+
+}

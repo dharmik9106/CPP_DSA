b1390c5508f79b27d233fe2bbee8b58dcbb3b71e
Dharmik Ahir <sangadharmik78@gmail.com>
2025-06-06 11:25:33 +0530

    oprater c++

---
+++ 02 CPP OPRATER/index.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+
+using namespace  std;
+
+int main(){
+
+
+    int a=10;
+    int b=30;
+
+    // cout << (a < b) << endl;
+    // cout << (a > b) << endl;
+    // cout << (a <= b) << endl;
+    // cout << (a >= b) << endl;
+    // cout << (a -= b) << endl;
+    // cout << (a -= b) << endl;
+
+
+    int z=20;
+    int x=20;
+
+
+    // cout <<( a==b && z==x) << endl;0
+    // cout <<( a==b || z==x) << endl;1
+  
+
+
+    int s= 30;
+    int t= 30;
+
+
+    // cout << !z << endl;
+    // cout << z << endl;
+
+
+    // cout << (s += t) << endl;
+    // cout << (s -= t) << endl;
+
+    int r=100;
+    int e=200;
+
+    // cout << (r -= e) << endl;
+
+
+    // char  c= 'a';
+
+    // cout << sizeof (r) << endl;
+    // cout << sizeof (c) << endl;
+
+
+    float  d= 3.5;
+    int  o=7;
+     
+    cout << (int) d +  o << endl;
+    return 0;
+}

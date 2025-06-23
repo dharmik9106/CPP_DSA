74ef5158d15e07af9c9d97091839b0de2a219506
Dharmik Ahir <sangadharmik78@gmail.com>
2025-06-23 12:40:35 +0530

    pointer pass and chain referance

---
+++ 19 CPP POINTER PASS/pasingarray.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void fun(int &arr)
+{
+    cout << &arr << endl;
+    
+}
+
+int main() 
+{
+
+int arr[5]= { 1, 2, 3, 4, 5};
+
+fun(*arr);
+cout << arr<< endl;
+
+
+    return 0;
+}
---
+++ 19 CPP POINTER PASS/pasingpara.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+
+void square(int z){
+    z= z*z;
+    cout << z << endl;
+
+}
+
+
+ void cube(int &n){
+    n = n*n*n;
+    cout << n <<endl;
+ }
+
+
+int main()
+{
+    // PASS BY VALUE 
+
+    int z =20;
+
+
+    square(z);
+    cout << z << endl;
+
+    cout << "_______________" << endl;
+
+
+
+    // PASS BY REFEREFCNE
+
+
+    int n =5;
+    int &ref =n;
+
+    cube(ref);
+
+    cout << n << endl;
+
+
+    return 0;
+}
